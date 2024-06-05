0f57d352c24d8cd2fa28cff0927f8dc7cef20ac8
HaiqaAhtsham <138617805+HaiqaAhtsham@users.noreply.github.com>
2024-06-05 19:03:22 +0000

    updated the portfolio msg

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
+  name: "Haiqa Ahtsham",
   title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  email: "haiqaahtsham@gmail.com",
+  gitHub: "HaiqaAhtsham",
+  instagram: "haiqa.aht",
+  linkedIn: "HaiqaAhtsham",
   medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  twitter: "",
+  youTube: "HaiqaAhtsham",
 };
 
 const primaryColor = "#4E567E";
